02ecca67f975bfee475fbe4f470f176e197d65af
handsleyj <handsley@lancaster.ac.uk>
2024-03-04 16:40:18 +0000

    Added JPanel and tiles as buttons, attempted actionPerformed method

--- Week18/TilePuzzle.java
+++ Week18/TilePuzzle.java
@@ -1,9 +1,32 @@
-public class TilePuzzle {
-    private TilePiece[] tiles;
-    private TilePiece blankTile;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TilePuzzle extends JPanel implements ActionListener{
+    private TilePiece[] tiles = new TilePiece[12];
+    private TilePiece blankTile = new TilePiece("blank_tile.jpg", 0, 0);
+    private JFrame frame = new JFrame();
 
     public TilePuzzle() {
-        tiles = new TilePiece[12];
-        blankTile = "blank_tile.jpg";
+        this.frame.setSize(475, 400);
+        GridLayout layout = new GridLayout(3, 4);
+        for (int i = 0; i < 11; i++) {
+            String filename = "tile_" + i + ".jpg";
+            tiles[i] = new TilePiece(filename, 0, 0);
+            this.tiles[i].addActionListener(this);
+            frame.add(tiles[i]);
+        }
+        this.frame.setLayout(layout);
+        this.frame.add(blankTile);
+        this.frame.setVisible(true);
+    }
+
+    //git testing...
+    public void actionPerformed(ActionEvent e) {
+        TilePiece t = (TilePiece)e.getSource();
+        if (blankTile.isAdjacentTo(t)) {
+            blankTile.exchangeImageWith(t);
+        }
     }
 }
