b8a1a9300311eb8e0458758ff8b4a911ac20b38d
handsleyj <162147305+handsleyj@users.noreply.github.com>
2024-03-04 15:34:39 +0000

    Add files via upload

---
+++ Week18/TilePuzzle.java
@@ -0,0 +1,9 @@
+public class TilePuzzle {
+    private TilePiece[] tiles;
+    private TilePiece blankTile;
+
+    public TilePuzzle() {
+        tiles = new TilePiece[12];
+        blankTile = "blank_tile.jpg";
+    }
+}
